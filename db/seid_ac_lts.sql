9b90858d28bc467cb60a65ae0df9a2b30d992603
Muhammad Raffi <raffikecil85@gmail.com>
2026-07-07 16:58:07 +0700

    Update DB

--- db/seid_ac_lts.sql
+++ db/seid_ac_lts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2026 at 09:43 AM
+-- Generation Time: Jul 07, 2026 at 11:57 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -30,15 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `action_master` (
   `action_id` int(11) NOT NULL,
   `action_name` varchar(150) NOT NULL,
+  `area` enum('IDU','ODU') NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `action_master`
---
-
-INSERT INTO `action_master` (`action_id`, `action_name`, `created_at`) VALUES
-(1, 'Change Coupler, Revacuum, Recharging', '2026-05-26 10:21:15');
 
 -- --------------------------------------------------------
 
@@ -52,13 +46,6 @@ CREATE TABLE `category_master` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `category_master`
---
-
-INSERT INTO `category_master` (`category_id`, `category_name`, `created_at`) VALUES
-(1, 'Part', '2026-05-26 10:18:47');
-
 -- --------------------------------------------------------
 
 --
@@ -68,15 +55,9 @@ INSERT INTO `category_master` (`category_id`, `category_name`, `created_at`) VAL
 CREATE TABLE `defect_master` (
   `defect_id` int(11) NOT NULL,
   `defect_name` varchar(100) NOT NULL,
+  `area` enum('IDU','ODU') NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `defect_master`
---
-
-INSERT INTO `defect_master` (`defect_id`, `defect_name`, `created_at`) VALUES
-(1, 'Noise', '2026-05-26 10:17:33');
 
 -- --------------------------------------------------------
 
@@ -88,6 +69,7 @@ CREATE TABLE `line_drop_transaction` (
   `transaction_id` int(11) NOT NULL,
   `product_id` varchar(100) NOT NULL,
   `model_code` varchar(20) NOT NULL,
+  `area` enum('IDU','ODU') NOT NULL,
   `defect_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `rootcause_id` int(11) NOT NULL,
@@ -98,14 +80,6 @@ CREATE TABLE `line_drop_transaction` (
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `line_drop_transaction`
---
-
-INSERT INTO `line_drop_transaction` (`transaction_id`, `product_id`, `model_code`, `defect_id`, `category_id`, `rootcause_id`, `action_id`, `remark`, `created_by`, `created_name`, `created_at`) VALUES
-(1, '5195DBZ3AHA5BEY2', 'AHA5BEY2', 1, 1, 1, 1, 'Bocor dibagian seal dan chuck', 'operator01', 'Operator IDU', '2026-05-26 10:34:42'),
-(2, '5195DBZ3AHA5BEY2', 'AHA5BEY2', 1, 1, 1, 1, 'Test', 'repair01', 'Repair', '2026-05-28 13:38:18');
-
 -- --------------------------------------------------------
 
 --
@@ -115,16 +89,9 @@ INSERT INTO `line_drop_transaction` (`transaction_id`, `product_id`, `model_code
 CREATE TABLE `model_master` (
   `model_id` int(11) NOT NULL,
   `model_name` varchar(100) NOT NULL,
+  `area` enum('IDU','ODU') NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `model_master`
---
-
-INSERT INTO `model_master` (`model_id`, `model_name`, `created_at`) VALUES
-(1, 'AHA5BEY2', '2026-05-26 10:07:07'),
-(2, 'AHA5BMY2', '2026-05-26 10:07:15');
 
 -- --------------------------------------------------------
 
@@ -135,15 +102,9 @@ INSERT INTO `model_master` (`model_id`, `model_name`, `created_at`) VALUES
 CREATE TABLE `rootcause_master` (
   `rootcause_id` int(11) NOT NULL,
   `rootcause_name` varchar(150) NOT NULL,
+  `area` enum('IDU','ODU') NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `rootcause_master`
---
-
-INSERT INTO `rootcause_master` (`rootcause_id`, `rootcause_name`, `created_at`) VALUES
-(1, 'Leak Coupler', '2026-05-26 10:20:54');
 
 -- --------------------------------------------------------
 
@@ -157,8 +118,7 @@ CREATE TABLE `user_master` (
   `password` varchar(255) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
   `area` enum('IDU','ODU') NOT NULL,
-  `process` varchar(32) NOT NULL,
-  `role` enum('OPERATOR','REPAIRMAN','LEADER','ADMIN') NOT NULL,
+  `role` enum('REPAIRMAN','LEADER','ADMIN') NOT NULL,
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -166,11 +126,11 @@ CREATE TABLE `user_master` (
 -- Dumping data for table `user_master`
 --
 
-INSERT INTO `user_master` (`user_id`, `username`, `password`, `name`, `area`, `process`, `role`, `created_at`) VALUES
-(1, 'admin01', 'SeidMail01', 'Admin', 'IDU', 'Structural Inspection', 'ADMIN', '2026-05-26 08:46:46'),
-(3, 'leader01', 'SeidMail01', 'Leader 1', 'IDU', 'ALL', 'LEADER', '2026-05-26 10:00:44'),
-(4, 'operator01', 'SeidMail01', 'Operator IDU', 'IDU', 'Structural Inspection', 'OPERATOR', '2026-05-26 10:33:36'),
-(6, 'repair01', 'SeidMail01', 'Repair', 'IDU', 'All', 'REPAIRMAN', '2026-05-28 13:36:00');
+INSERT INTO `user_master` (`user_id`, `username`, `password`, `name`, `area`, `role`, `created_at`) VALUES
+(1, 'admin01', 'SeidMail01', 'Admin', 'IDU', 'ADMIN', '2026-05-26 08:46:46'),
+(6, 'repairman_idu_a', 'SeidMail01', 'Repairman', 'IDU', 'REPAIRMAN', '2026-05-28 13:36:00'),
+(7, 'leader_idu_a', 'SeidMail01', 'Dewa & Rifki', 'IDU', 'LEADER', '2026-07-07 14:00:15'),
+(8, 'leader_odu_a', 'SeidMail01', 'Irwanto', 'ODU', 'LEADER', '2026-07-07 14:00:36');
 
 --
 -- Indexes for dumped tables
@@ -245,13 +205,13 @@ ALTER TABLE `defect_master`
 -- AUTO_INCREMENT for table `line_drop_transaction`
 --
 ALTER TABLE `line_drop_transaction`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `model_master`
 --
 ALTER TABLE `model_master`
-  MODIFY `model_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `model_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `rootcause_master`
@@ -263,7 +223,7 @@ ALTER TABLE `rootcause_master`
 -- AUTO_INCREMENT for table `user_master`
 --
 ALTER TABLE `user_master`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
